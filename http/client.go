c10fef8512c99a5aa9b94bae8440504fac2050dd
yushijun <yushijun110@126.com>
2019-01-04 19:08:01 +0800

    add http client

---
+++ http/client.go
@@ -0,0 +1,89 @@
+package http
+
+import (
+    "io"
+    "time"
+    "bytes"
+    "net/http"
+    "net/url"
+    "encoding/json"
+    "encoding/xml"
+
+    "github.com/JoveYu/zgo/log"
+)
+
+type Client struct {
+    *http.Client
+}
+
+func NewClient() *Client {
+    return &Client{
+        Client: &http.Client{},
+    }
+}
+
+func (c *Client) timit(start time.Time, resp *http.Response, err error) {
+    req := resp.Request
+    if err == nil {
+        log.Info("ep=httpclient|method=%s|url=%s|code=%d|body=%d|time=%d",
+            req.Method, req.URL, resp.StatusCode, resp.ContentLength,
+            time.Now().Sub(start) / time.Microsecond,
+        )
+    } else {
+        log.Warn("ep=httpclient|method=%s|url=%s|code=%d|body=%d|time=%d|err=%s",
+            req.Method, req.URL, 0, 0,
+            time.Now().Sub(start) / time.Microsecond, err,
+        )
+    }
+}
+
+func (c *Client) Do(req *http.Request) (*http.Response, error) {
+    start := time.Now()
+    resp, err :=  c.Client.Do(req)
+    c.timit(start, resp, err)
+    return resp, err
+}
+
+func (c *Client) Get(url string) (*http.Response, error) {
+    start := time.Now()
+    resp, err := c.Client.Get(url)
+    c.timit(start, resp, err)
+    return resp, err
+}
+
+func (c *Client) Head(url string) (*http.Response, error) {
+    start := time.Now()
+    resp, err := c.Client.Head(url)
+    c.timit(start, resp, err)
+    return resp, err
+}
+
+func (c *Client) Post(url string, contentType string, body io.Reader) (*http.Response, error) {
+    start := time.Now()
+    resp, err := c.Client.Post(url, contentType, body)
+    c.timit(start, resp, err)
+    return resp, err
+}
+
+func (c *Client) PostForm(url string, data url.Values) (*http.Response, error) {
+    start := time.Now()
+    resp, err := c.Client.PostForm(url, data)
+    c.timit(start, resp, err)
+    return resp, err
+}
+
+func (c *Client) PostJson(url string, v interface{}) (*http.Response, error) {
+    data, err := json.Marshal(v)
+    if err != nil {
+        return nil, err
+    }
+    return c.Post(url, "application/json", bytes.NewReader(data))
+}
+
+func (c *Client) PostXml(url string, v interface{}) (*http.Response, error) {
+    data, err := xml.Marshal(v)
+    if err != nil {
+        return nil, err
+    }
+    return c.Post(url, "application/xml", bytes.NewReader(data))
+}
---
+++ http/client_test.go
@@ -0,0 +1,57 @@
+package http
+
+import "io/ioutil"
+import "strings"
+import "testing"
+import "net/http"
+import "net/url"
+import "encoding/xml"
+import "github.com/JoveYu/zgo/log"
+
+func TestClient(t *testing.T) {
+    log.Install("stdout")
+    client := NewClient()
+    req,err := http.NewRequest("GET", "http://httpbin.org/get", nil)
+    if err != nil {
+        log.Error(err)
+    }
+    resp,_ := client.Do(req)
+    body,_ := ioutil.ReadAll(resp.Body)
+    log.Debug("%s", body)
+
+    resp,_ = client.Get("http://httpbin.org/get")
+    body,_ = ioutil.ReadAll(resp.Body)
+    log.Debug("%s", body)
+
+    resp,_ = client.Head("http://httpbin.org/get")
+    body,_ = ioutil.ReadAll(resp.Body)
+    log.Debug("%s", body)
+
+    resp,_ = client.Post("http://httpbin.org/post", "application/json",  strings.NewReader("{\"key\":\"value\"}"))
+    body,_ = ioutil.ReadAll(resp.Body)
+    log.Debug("%s", body)
+
+    resp,_ = client.PostForm("http://httpbin.org/post", url.Values{
+        "key": []string{"1", "2"},
+    })
+    body,_ = ioutil.ReadAll(resp.Body)
+    log.Debug("%s", body)
+
+    resp,_ = client.PostJson("http://httpbin.org/post", map[string]int{"key": 1} )
+    body,_ = ioutil.ReadAll(resp.Body)
+    log.Debug("%s", body)
+
+    type User struct {
+        XMLName xml.Name `xml:"xml"`
+        Name string `xml:"name"`
+        Id int `xml:"id,attr"`
+    }
+    user := User{
+        Name: "test",
+        Id: 1,
+    }
+    resp,_ = client.PostXml("http://httpbin.org/post", user)
+    body,_ = ioutil.ReadAll(resp.Body)
+    log.Debug("%s", body)
+
+}
